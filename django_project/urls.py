e07293cfc62fa638802cbdba90c9075dfc78430a
seetharamugn <55573879+seetharamugn@users.noreply.github.com>
2021-01-13 13:34:12 +0530

    Update urls.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,6 +20,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
@@ -47,6 +50,10 @@ urlpatterns = [
          ),
          name='password_reset_complete'),
     path('', include('blog.urls')),
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+]
 ]
 
 
